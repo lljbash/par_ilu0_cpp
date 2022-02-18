e3bf9d72207f4ddfdf0b4074efa289211575de1a
Li Lingjie <lljbash@gmail.com>
2022-02-18 12:30:36 +0800

    split procedures

--- par_ilu0_c.h
+++ par_ilu0_c.h
@@ -1,13 +1,23 @@
 #ifndef PAR_ILU0_C_H
 #define PAR_ILU0_C_H
 
+#ifndef __cplusplus
+#include <stdbool.h>
+#endif
+
 #include "itsol/globheads.h"
 
 #ifdef __cplusplus
 extern "C" {
 #endif
 
-int par_ilu0_c(csptr csmat, iluptr lu);
+typedef void* ParILU0SolverHandler;
+
+ParILU0SolverHandler par_ilu0_create_solver(int nthreads);
+void par_ilu0_destroy_solver(ParILU0SolverHandler hdl);
+void par_ilu0_import_matrix(ParILU0SolverHandler hdl, csptr csmat);
+int par_ilu0_factorize(ParILU0SolverHandler hdl, bool different_structure);
+void par_ilu0_export_matrix(ParILU0SolverHandler hdl, iluptr lu);
 
 #ifdef __cplusplus
 }
